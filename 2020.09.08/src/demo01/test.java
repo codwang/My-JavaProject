e6d7ca52fe3abfa2152f62896269182b099e4f10
codwang <1309357424@qq.com>
2020-09-24 22:43:10 +0800

    java

---
+++ 2020.09.08/src/demo01/test.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package demo01;
+
+import java.util.Scanner;
+
+/**
+ * @author 13093
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("打印命令行参数");
+//		for(int i=0;i<args.length;i++)
+//		{
+//			System.out.println(args[i]);
+//		}
+//		long b=300L;
+//		byte a=5;
+//		short  s1=6,s2=0;
+//		a=(byte)s2;
+//		s2=s1;
+//		int a=-10,b,c,d;
+//		b=a<<2;
+//		c=a>>2;
+//		d=a>>>2;
+//		int sum=0;
+//		int a[]=new int[100];
+//		for(int i=0;i<100;i++)
+//		{
+//			a[i]=i+1;
+//		}
+//		for(int e:a)
+//		{
+//			sum+=e;
+//		}
+		//斐波那契数列
+//		int fib[]=new int[40];
+//		fib[0]=1;
+//		fib[1]=1;
+//		for(int i=2;i<40;i++)
+//		{
+//			fib[i]=fib[i-1]+fib[i-2];
+//		}
+//		for(int i=0;i<40;i++)
+//		{
+//			System.out.print(fib[i]+" ");
+//			if(i!=0&&i%5==0)
+//			{
+//				System.out.println();
+//			}
+//		}
+		//System.out.println();
+//		int i=1,j=1,k=1;
+//	
+//		if(5*i+3*j+(1/3)*k<=100&&i+j+k<=100)
+//		{
+//			System.out.println(i);
+//			System.out.println(j);
+//			System.out.println(k);
+//		}
+		
+		//System.out.println();
+		
+	}
+
+}
